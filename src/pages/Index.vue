8255294892584fc7f7264f03d30aced9b26e274d
Anne Bonfante <bonfante.anne@gmail.com>
2021-11-07 21:03:13 +0100

    commit of despair

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,96 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row">
+      <div
+        v-if="!showCamera"
+        class="col-12 text-center q-pt-md"
+      >
+
+      </div>
+    </div>
+    <div class="row justify-center q-pt-lg">
+       <div class="col-12 text-center">
+        <span class="text-subtitle2 text-grey-9">
+          {{ textInfo }}
+        </span>
+        <q-btn color="blue-grey-10" rounded icon="camera_alt" label="Read QRCode"
+          class="full-width" size="lg" @click="turnCameraOn()"
+          v-show="!showCamera"/>
+
+          <p class="text-subtitle1" v-if="result">Last result: <b>{{ result }}</b></p>
+          <div v-if="showCamera">
+            <qrcode-stream :camera="camera" @decode="onDecode">
+            </qrcode-stream>
+          </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<style>
+</style>
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+<script>
+import { QrcodeStream } from "vue-qrcode-reader"
+// import { QrcodeStream } from 'vue-qrcode-reader/src/index'
+
+export default {
+  name: 'PageIndex',
+  components: { QrcodeStream },
+  data () {
+    return {
+      isValid: undefined,
+      camera: 'auto',
+      result: null,
+      showCamera: false
+    }
+  },
+  computed: {
+    textInfo () {
+      return this.showCamera ? 'position the qrcode on the camera' : 'Press the button and scan a qrcode.'
+    }
+  },
+  methods: {
+    async onDecode (content) {
+      this.result = content
+      this.turnCameraOff()
+    },
+
+    turnCameraOn () {
+      this.camera = 'auto'
+      this.showCamera = true
+    },
+
+    turnCameraOff () {
+      this.camera = 'off'
+      this.showCamera = false
+    }
+  }
+}
 </script>
+
+<style scoped>
+/* .validation-success,
+.validation-failure,
+.validation-pending {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, .8);
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.4rem;
+  padding: 10px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+.validation-success {
+  color: green;
+}
+.validation-failure {
+  color: red;
+} */
+</style>
